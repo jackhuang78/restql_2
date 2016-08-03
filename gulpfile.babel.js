7f55dbe924a30659e9557a435a9697f541aeceaf
Jack Huang <jack.huang78@gmail.com>
2016-08-03 01:24:53 -0700

    Setup spec

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,13 +1,23 @@
+require('babel-core/register');
 import gulp from 'gulp';
+import mocha from 'gulp-mocha';
 import eslint from 'gulp-eslint';
+import {argv} from 'yargs';
+
 
 gulp.task('default', () => {
-	console.log('Hello World!')
-})
+	console.log('Hello World!');
+});
 
 gulp.task('lint', () => {
-	return gulp.src(['src/**/*.js', 'test/**/*.js', './*.js'])
+	return gulp.src(['src/**/*.js', 'spec/**/*.js', './*.js'])
 		.pipe(eslint())
 		.pipe(eslint.format())
 		.pipe(eslint.failAfterError());
+});
+
+gulp.task('test', ['lint'], () => {
+	let file = (argv.f != null) ? argv.f : '**/*';
+	return gulp.src(`spec/${file}.js`)
+		.pipe(mocha({grep: argv.grep}));
 });
---
+++ spec/RestQLTest.js
@@ -0,0 +1,11 @@
+import chai,{expect} from 'chai';
+
+import RestQL from '../src/RestQL';
+
+describe('# RestQL', () => {
+	describe('# constructor', () => {
+		it('should create RestQL instance', () => {
+			expect(new RestQL()).to.be.not.null;
+		});
+	});
+});
