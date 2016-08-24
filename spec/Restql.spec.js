9e3c0bc320c037719ffe5e4e8883c3e194c4e627
Jack Huang <jack.huang78@gmail.com>
2016-08-23 23:55:12 -0700

    Use enum for conditional operator

--- spec/Restql.spec.js
+++ spec/Restql.spec.js
@@ -1,10 +1,65 @@
 import chai,{expect} from 'chai';
-import Restql from '../src/Restql';
+import Restql,{CondOp} from '../src/Restql';
 
 describe('# Restql', () => {
 	describe('# constructor', () => {
 		it('should create empty Restql instance', () => {
 			expect(new Restql()).to.be.not.null;
 		});
+	});
+
+	describe('# parseQueryParams', () => {
+		let restql = null;
+		beforeEach(() => {
+			restql = new Restql();
+		});
+
+		it('should parse basic query', () => {
+			expect(restql.parseQueryParams({
+				f1:'1', 
+				f2:'', 
+				f3:'null', 
+				f4:'undefined'
+			})).to.deep.equal({
+				f1:[CondOp.EQ,'1'], 
+				f2:[CondOp.EQ,undefined], 
+				f3:[CondOp.EQ,null], 
+				f4:[CondOp.EQ,undefined]
+			});
+		});
+
+		it('should parse query with quotes', () => {
+			expect(restql.parseQueryParams({
+				f1:'"1"', 
+				f2:'""', 
+				f3:'"null"', 
+				f4:'"undefined"'
+			})).to.deep.equal({
+				f1:[CondOp.EQ,'1'], 
+				f2:[CondOp.EQ,''], 
+				f3:[CondOp.EQ,'null'], 
+				f4:[CondOp.EQ,'undefined']
+			});
+		});
+
+		it('should parse query with conditional operator', () => {
+			expect(restql.parseQueryParams({
+				f1:'==1', 
+				f2:'!=2', 
+				f3:'>3', 
+				f4:'>=4', 
+				f5:'<5', 
+				f6:'<=6'
+			})).to.deep.equal({
+				f1:[CondOp.EQ,'1'], 
+				f2:[CondOp.NE,'2'], 
+				f3:[CondOp.GT,'3'], 
+				f4:[CondOp.GE,'4'],
+				f5:[CondOp.LT,'5'], 
+				f6:[CondOp.LE,'6']
+			});
+		});
+
+		
 	});
 });
--- src/Restql.js
+++ src/Restql.js
@@ -1,7 +1,64 @@
-class Restql {
-	constructor(paths, headers, body) {
+import {expect} from 'chai';
+import {Enum} from 'enumify';
 
+class CondOp extends Enum {}
+CondOp.initEnum({
+	EQ: { sym: '==' },
+	NE: { sym: '!=' },
+	GE: { sym: '>=' },
+	GT: { sym: '>'  },
+	LE: { sym: '<=' },
+	LT: { sym: '<'  }
+});
+CondOp.get = (sym) => {
+	switch(sym.substring(0,2)) {
+		case CondOp.EQ.sym: return [CondOp.EQ, sym.substring(2)];
+		case CondOp.NE.sym: return [CondOp.NE, sym.substring(2)];
+		case CondOp.GE.sym: return [CondOp.GE, sym.substring(2)];
+		case CondOp.LE.sym: return [CondOp.LE, sym.substring(2)];
+	}
+	switch(sym.substring(0,1)) {
+		case CondOp.GT.sym: return [CondOp.GT, sym.substring(1)];
+		case CondOp.LT.sym: return [CondOp.LT, sym.substring(1)];
+	}
+	return [CondOp.EQ, sym];
+};
+
+class Restql {
+	constructor() {
+		
+	}
+
+	parseQueryParams(queryParams) {
+		let obj = {};
+
+		for(let field in queryParams) {
+			let value = queryParams[field];
+			let condOp = null;
+
+			[condOp, value] = CondOp.get(value);
+
+
+			switch(value) {
+				case '':
+				case 'undefined':
+				 obj[field] = [condOp, undefined];
+				 break;
+
+				case 'null':
+					obj[field] = [condOp, null];
+					break;
+
+				default:
+					if(value.length >= 2 && value[0] == '"' && value[value.length - 1] == '"')
+						value = value.slice(1, -1);
+					obj[field] = [condOp, value];
+			}
+			
+		}
+
+		return obj;
 	}
 }
 
-export default Restql;
+export {Restql as default, CondOp};
